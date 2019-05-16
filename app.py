13be1fdb0d1ed966683eb9d3b2c3845eaa59f7fe
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2019-05-17 04:56:24 +0700

    app

--- app.py
+++ app.py
@@ -526,7 +526,6 @@ def api_message():
                     (general_message, data["message"], 0))
                 con.commit()
                 logging.debug("PAIR SENTENCE CREATED")
-            if
 
         general_message = tag(data["message"])
         logging.info("Normalization: {}".format(general_message))
