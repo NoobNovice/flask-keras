f14e962df653c3eda5e7ec66b08fd19d80e609da
NoobNovice <30790608+NoobNovice@users.noreply.github.com>
2019-05-19 14:51:24 +0700

    app

--- app.py
+++ app.py
@@ -386,6 +386,7 @@ def api_message():
             temp = cur.fetchone()
             RO = temp[0]
             RES_NAME = int(data["res_topic"])
+            ID = int(data["res_topic"])
         elif RES_NAME != "":
             try:
                 logging.info("Search form name: {}".format(RES_NAME))
@@ -442,18 +443,18 @@ def api_message():
     elif predict_result == 5:
         logging.debug("LOCATION CASE")
         RL = None
-        if RES_NAME == "" and data["res_topic"] != -1:
+        if RES_NAME == "" and int(data["res_topic"]) != -1:
             logging.info("Search form res_id: {}".format(data["res_topic"]))
             cur.execute("SELECT address FROM restaurant_info WHERE id=%s",(data["res_topic"]))
             temp = cur.fetchone()
             RL = temp[0]
-            RES_NAME = data["res_topic"]
+            RES_NAME = int(data["res_topic"])
         elif RES_NAME != "":
             try:
                 logging.info("Search form name: {}".format(RES_NAME))
                 cur.execute("SELECT address,id FROM restaurant_info WHERE name LIKE %s",('%'+RES_NAME+'%'))
                 temp = cur.fetchone()
-                RES_NAME = temp[1]
+                RES_NAME = int(temp[1])
                 RL = temp[0]   
             except Exception as e:
                 RES_NAME = -1
@@ -472,10 +473,7 @@ def api_message():
             sending_message = RL
             cur.close()
             log_id = create_logs(data["message"], sending_message, data["userID"], "")
-            if RES_NAME is not int:
-                RES_NAME = -1
-            if MENU is not int:
-                MENU = -1
+            MENU = -1
             logging.debug("LOG CREATED")
             logging.info("res_id: {}".format(RES_NAME))
             logging.info("previous: {}".format(data["message"]))
